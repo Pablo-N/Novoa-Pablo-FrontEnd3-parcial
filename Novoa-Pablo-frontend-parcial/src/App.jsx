364ac7aa7145e5940b1b36104bfc09d1efc870c9
Pablo-N <105933981+Pablo-N@users.noreply.github.com>
2022-11-10 16:55:33 -0500

    parcial up
    parcial up

--- Novoa-Pablo-frontend-parcial/src/App.jsx
+++ Novoa-Pablo-frontend-parcial/src/App.jsx
@@ -1,32 +1,29 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Estatus from './components/Estatus'
+import Posteos from './components/Posteos'
+
+// El componente App es el padre de:
+// - Estatus
+// - Posteos
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de likes.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Post.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos para que manipulen o lean su estado.
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const countHandler = (likes) => {
+    setCount(count + likes);
+  };
+
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Estatus count={count}  />
+     <Posteos countHandler={countHandler}/>    
+      
     </div>
   )
 }
